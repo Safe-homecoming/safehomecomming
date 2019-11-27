ca6b927780ca99b786a9ddac0c64b03da6aff5c1
YOUNGJUYOON1992 <dudwntld@gmail.com>
2019-11-28 07:42:03 +0900

    로그인 기능 완성

--- app/src/main/java/com/example/safehomecoming/retrifit_setup/Resultm.java
+++ app/src/main/java/com/example/safehomecoming/retrifit_setup/Resultm.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public class Resultm {
 
+    // 기본으로 보여주는 json 결과값
     private  String result;
     private  String result_code;
     private  String description;
     private  int result_row;
 
+    // 회원 로그인시
+    private String name;
+    private String memtype;
+    private String gender;
+    private String wstatus;
 
-    private List<ItemRequest> items;
+    // 시민 요청 현황 list  조회시
+    private List<ItemRequest> items;// 안심이 수락 가능 현황list  하위
+
+
 
     public List<ItemRequest> getItems() {
         return items;
@@ -50,5 +59,38 @@ public class Resultm {
 
     public void setResult_row(int result_row) {
         this.result_row = result_row;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMemtype() {
+        return memtype;
+    }
+
+    public void setMemtype(String memtype) {
+        this.memtype = memtype;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getWstatus() {
+        return wstatus;
+    }
+
+    public void setWstatus(String wstatus) {
+        this.wstatus = wstatus;
     }
 }
